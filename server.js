00b27b3bd601409960234c6b00bb83ad91b724d3
Sonika Gupta <sonikag0209@gmail.com>
2023-08-11 16:27:50 -0600

    mongoDB

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const mongoConfig = require("./config");
+
+let pokemon = require("./models/pokemon");
+
+const Pokemons = require("./models/Pokemons");
+
+const app = express();
+
+const PORT = 3001;
+
+mongoConfig();
+
+// Load the create engine function
+const jsxEngine = require("jsx-view-engine");
+
+// load the method override function
+const methodOverride = require("method-override");
+
+// A middleware that formats the data into an object we can use on req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Configure the view engine and look for files ending in jsx
+app.set("view engine", "jsx");
+
+// Create the engine and accept files ending in jsx
+app.engine("jsx", jsxEngine());
+
+// give our form more HTTP methods to work with like DELETE and PUT
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  // console.log(pokemon)
+  res.send("Welcome to the pokemon app");
+});
+
+app.get("/pokemon", async (req, res) => {
+  let pokemonsData;
+  try {
+    pokemonsData = await Pokemons.find();
+    console.log("got to db ", pokemonsData);
+  } catch (err) {
+    console.log("err: " + err);
+  }
+
+  res.render("Index", { pokemon: pokemonsData });
+});
+
+app.get("/pokemon/new", async (req, res) => {
+  res.render("New");
+});
+
+app.delete("/pokemon/:id", async (req, res) => {
+  try {
+    await Pokemons.findByIdAndDelete(req.params.id);
+  } catch (err) {
+    console.log("err delete: " + err);
+  }
+  res.redirect("/pokemon");
+});
+
+app.post("/pokemon", async (req, res) => {
+  let pokemon = req.body;
+  pokemon.img = `http://img.pokemondb.net/artwork/${pokemon.name.toLowerCase()}`;
+
+  try {
+    await Pokemons.create(pokemon);
+    console.log(pokemon);
+  } catch (err) {
+    console.log("err post: " + err);
+  }
+
+  res.redirect("/pokemon");
+});
+
+app.get("/pokemon/:id", async (req, res) => {
+  try {
+    let pokemon = await Pokemons.findById(req.params.id);
+    res.render("Show", { pokemon });
+  } catch (err) {
+    console.log("err get: " + err);
+    res.redirect("/pokemon");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at port: ${PORT}`);
+});
---
+++ views/Index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const myStyle = {
+  color: '#ffffff',
+  backgroundColor: '#000000',
+};
+
+export default function Index({ pokemon }) {
+  return (
+    <div style={myStyle}>
+      <h1>See all the pokemon!</h1>
+      <ul>
+        {pokemon.map((poke) => <li key={poke._id}><a href={`/pokemon/${poke._id}`}>{poke.name[0].toUpperCase() + poke.name.slice(1)}</a></li>)}
+      </ul>
+
+      <form action="/pokemon/new">
+        <button>New</button>
+      </form>
+    </div>
+  )
+}
